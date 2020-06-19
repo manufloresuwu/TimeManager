02b583515beebb13319f1d57fa132fff56d70af1
manuflores <flores.morales.manuela@gmail.com>
2020-06-18 21:17:59 -0400

    3 clases nuevas user respository (errores) user (atributos, getters, setters) splashactivity 1 nuevo xml array (generos)

--- app/src/main/java/com/manuela/timemanager/MainActivity.java
+++ app/src/main/java/com/manuela/timemanager/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.manuela.timemanager.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
