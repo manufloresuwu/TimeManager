4667ed23dbab6fd940eea97711a3aa0b09c0169f
Nachin7u7 <ignacio.valencia.sauma@gmail.com>
2020-06-20 18:01:13 -0400

    boton con pop

--- app/src/main/java/com/manuela/timemanager/MainActivity.java
+++ app/src/main/java/com/manuela/timemanager/MainActivity.java
@@ -3,6 +3,7 @@ package com.manuela.timemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +11,7 @@ import android.widget.Button;
 import com.manuela.timemanager.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +19,12 @@ public class MainActivity extends AppCompatActivity {
 
         Button start = (Button) findViewById(R.id.button);
 
+        mp = MediaPlayer.create(this,R.raw.pop);
+
         start.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                mp.start();
                 Intent intent= new Intent (MainActivity.this, CalendarActivity.class);
                 startActivity(intent);
             }
