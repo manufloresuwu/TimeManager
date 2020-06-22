4808036e1998c54448d897fb17dc3fa261b221fa
Nachin7u7 <ignacio.valencia.sauma@gmail.com>
2020-06-22 19:46:19 -0400

    fix del CustomArrayAdapter 2

--- app/src/main/java/com/manuela/timemanager/AddTaskActivity.java
+++ app/src/main/java/com/manuela/timemanager/AddTaskActivity.java
@@ -2,7 +2,11 @@ package com.manuela.timemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
 
 public class AddTaskActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class AddTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        Button ok = (Button) findViewById(R.id.button);
+        Button cancel = (Button) findViewById(R.id.cancel);
+
+        TimePicker timer = (TimePicker) findViewById(R.id.timer);
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddTaskActivity.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddTaskActivity.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
