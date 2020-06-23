e93d485fae6a44836ba597ce5cf06278eb28387d
manuflores <flores.morales.manuela@gmail.com>
2020-06-23 03:05:46 -0400

    YA NO QUIERO NADA

--- app/src/main/java/com/manuela/timemanager/ProfileActivity.java
+++ app/src/main/java/com/manuela/timemanager/ProfileActivity.java
@@ -1,6 +1,28 @@
 package com.manuela.timemanager;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSeekBar;
+
+import com.manuela.timemanager.UserRepository;
+import com.manuela.timemanager.User;
 
 public class ProfileActivity extends AppCompatActivity {
+    private Context context;
+    SharedPreferences sharedPref = context.getSharedPreferences(
+            "usuario", Context.MODE_PRIVATE);
+    private String user = sharedPref.getString("nombre", null);
 }
